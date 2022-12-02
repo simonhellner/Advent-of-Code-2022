98e6b66c60fcdf432969cc0d2521b9fffd0fc846
Simon Hellner <simon.hellner@ericsson.com>
2022-12-02 08:17:20 +0100

    Day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -2,8 +2,31 @@
 
 internal class Program
 {
+    static void Part1()
+    {
+        var input = File.ReadAllText("input.txt");
+        var elfs = input.Split(Environment.NewLine + Environment.NewLine);
+        var elfsTotalCalories = elfs.Select(x => x.Split(Environment.NewLine).Select(y => int.Parse(y)).Sum());
+        var maxCalories = elfsTotalCalories.Max();
+
+        Console.WriteLine($"Part 1: That Elf is carrying {maxCalories} calories");
+    }
+
+    static void Part2()
+    {
+        var input = File.ReadAllText("input.txt");
+        var elfs = input.Split(Environment.NewLine + Environment.NewLine);
+        var elfsTotalCalories = elfs.Select(x => x.Split(Environment.NewLine).Select(y => int.Parse(y)).Sum()).ToList();
+        
+        elfsTotalCalories.Sort();
+        var top3ElfsSum = elfsTotalCalories.GetRange(elfsTotalCalories.Count - 3, 3).Sum();
+
+        Console.WriteLine($"Part 2: The top 3 Elfs are carrying {top3ElfsSum} calories");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Part1();
+        Part2();
     }
 }
