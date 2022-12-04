ef776bef10b69a34a9cb5909c66d671da8cb25e7
Simon <85552673+simonhellner@users.noreply.github.com>
2022-12-04 09:48:10 +0100

    Day4

---
+++ Day4/Part1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace Day4;
+
+public static class Part1
+{
+    class SectionRange
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+
+    static bool RangeIsWithinRange(SectionRange range1, SectionRange range2)
+    {
+
+        if (range1.Start <= range2.Start)
+        {
+            if (range1.End >= range2.End)
+            {
+                return true;
+            }
+        }
+        if (range2.Start <= range1.Start)
+        {
+            if (range2.End >= range1.End)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static void Part1Main()
+    {
+        var input = File.ReadAllLines("input.txt");
+        var sectionRanges = input.Select(line =>
+        {
+            var sectionPairs = line.Split(',');
+            var pairs = sectionPairs.Select(sectionPair =>
+            {
+                var range = sectionPair.Split('-');
+                return new SectionRange()
+                {
+                    Start = int.Parse(range[0]),
+                    End = int.Parse(range[1])
+                };
+            });
+            return pairs.ToArray();
+        });
+
+        int counter = 0;
+        foreach (var s in sectionRanges)
+        {
+            var firstRange = s[0];
+            var secondRange = s[1];
+            if (RangeIsWithinRange(s[0], s[1]))
+            {
+                counter++;
+            }
+        }
+
+        Console.WriteLine($"[Part1] Answer: {counter}");
+    }
+}
+
---
+++ Day4/Part2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Day4;
+
+public static class Part2
+{
+
+    class SectionRange
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+
+    static bool RangeIsWithinRange(SectionRange range1, SectionRange range2)
+    {
+        if (range1.Start >= range2.Start && range1.Start <= range2.End) 
+        {
+            return true;
+        }
+
+        if(range2.End >= range1.Start && range2.End <= range1.Start)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void Part2Main()
+    {
+        var input = File.ReadAllLines("input.txt");
+        var sectionRanges = input.Select(line =>
+        {
+            var sectionPairs = line.Split(',');
+            var pairs = sectionPairs.Select(sectionPair =>
+            {
+                var range = sectionPair.Split('-');
+                return new SectionRange()
+                {
+                    Start = int.Parse(range[0]),
+                    End = int.Parse(range[1])
+                };
+            });
+            return pairs.ToArray();
+        });
+
+        int counter = 0;
+        foreach (var sectionRange in sectionRanges)
+        {
+            bool overlapping = RangeIsWithinRange(sectionRange[0], sectionRange[1]) || RangeIsWithinRange(sectionRange[1], sectionRange[0]);
+            if (overlapping)
+            {
+                counter++;
+            }
+        }
+
+        Console.WriteLine($"[Part2] Answer: {counter}");
+    }
+}
